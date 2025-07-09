f8d2aa31146ff6c6802b22a72365bc5da003c425
guruprasath04-byte <gurup8256@gmail.com>
2025-07-10 00:07:40 +0530

    new changes

--- environments/dev/variables.tf
+++ environments/dev/variables.tf
@@ -1,5 +1,5 @@
 variable "aws_region" {
   type        = string
   default     = "ap-southeast-1"
-  description = "which region AWS resources to be created "
+  description = "Which region AWS resources should be created in"
 }
