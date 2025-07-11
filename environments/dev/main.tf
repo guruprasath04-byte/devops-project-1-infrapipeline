6b56c7da37a25eddeadc847a3264c98b0e0c5287
guruprasath04-byte <gurup8256@gmail.com>
2025-07-11 23:13:24 +0530

    fix: renamed subnet_az to az to match vpc module

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -2,7 +2,7 @@ module "vpc" {
   source      = "../../modules/vpc"
   vpc_cidr    = "10.0.0.0/16"
   subnet_cidr = "10.0.1.0/24"
-  subnet_az   = "${var.aws_region}a"
+  az          = "${var.aws_region}a"
   env         = "dev"
 
 } 
